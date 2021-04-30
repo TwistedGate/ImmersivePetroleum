0614ff6b1a26f9512454e3f6d68d6e86b3cd0046
TwistedGate <31029668+TwistedGate@users.noreply.github.com>
2021-04-30 09:11:26 +0200

    Removed onBlockActivated
    Left over during creation

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/HydrotreaterBlock.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/HydrotreaterBlock.java
@@ -1,21 +1,9 @@
 package flaxbeard.immersivepetroleum.common.blocks;
 
 import flaxbeard.immersivepetroleum.common.blocks.tileentities.HydrotreaterTileEntity;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.ActionResultType;
-import net.minecraft.util.Hand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.World;
 
 public class HydrotreaterBlock extends IPMetalMultiblock<HydrotreaterTileEntity>{
 	public HydrotreaterBlock(){
 		super("hydrotreater", () -> HydrotreaterTileEntity.TYPE);
-	}
-	
-	@Override
-	public ActionResultType onBlockActivated(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult hit){
-		return super.onBlockActivated(state, world, pos, player, hand, hit);
 	}
 }
