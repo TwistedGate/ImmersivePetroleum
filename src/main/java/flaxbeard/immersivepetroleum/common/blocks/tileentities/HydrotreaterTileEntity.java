6d621e4a3f442509ac30633d33b7532b7d2725cd
BuildTools <unconfigured@null.spigotmc.org>
2022-06-02 11:46:28 -0400

    Hydrotreater now clears process queue before ticking

--- src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/HydrotreaterTileEntity.java
+++ src/main/java/flaxbeard/immersivepetroleum/common/blocks/tileentities/HydrotreaterTileEntity.java
@@ -212,6 +212,7 @@ public class HydrotreaterTileEntity extends PoweredMultiblockTileEntity<Hydrotre
 	
 	@Override
 	public void tick(){
+		super.tick();
 		checkForNeedlessTicking();
 		
 		if(this.world.isRemote || isDummy() || isRSDisabled()){
@@ -247,12 +248,12 @@ public class HydrotreaterTileEntity extends PoweredMultiblockTileEntity<Hydrotre
 				}
 			}
 		}
+
 		
 		if(!this.processQueue.isEmpty()){
 			update = true;
 		}
-		
-		super.tick();
+
 		
 		if(this.tanks[TANK_OUTPUT].getFluidAmount() > 0){
 			BlockPos outPos = getBlockPosForPos(Fluid_OUT).up();
@@ -276,6 +277,7 @@ public class HydrotreaterTileEntity extends PoweredMultiblockTileEntity<Hydrotre
 		if(update){
 			updateMasterBlock(null, true);
 		}
+		
 	}
 	
 	@Override
